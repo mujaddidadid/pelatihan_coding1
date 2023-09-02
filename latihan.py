7fb66209b2b5aade601a81c325273786f7fc8a98
mujaddidadid <143859254+mujaddidadid@users.noreply.github.com>
2023-09-02 10:45:50 +0700

    commit pertama

---
+++ latihan.py
@@ -0,0 +1,40 @@
+apel =52                    #tipe integer
+_apel =34
+print (apel + _apel)
+                             #tipe float
+apel = 53.2
+_apel =53.3
+print (apel + _apel)
+                            #tipe data str
+nama = "adid"
+umur = 17 
+tahun = 2005 
+                            #tipe data bool
+apel_benar = True
+apel_salah = False
+                            #tipe data list=menyimpan dta lebih dari satu
+ini_list = ['kuda','kambing','sapi']
+list_apel = ['manis',2,23.3]
+print (ini_list)
+print (list_apel)
+                            #tipe data tuple
+#tipe data untuk menyimpan lebih dari satu nilai,tetapi nilai di dalamnya tidak bisa di ubah
+#
+tuple_jumlah = (2, 23.3, 'satu')
+print (tuple_jumlah)
+                            #tipe data Dictionary
+#tipe data untuk menyimpan lebih dari satu nilai,ddi dalaam nya ada kuncinya sama value
+apel_dictionary = {'nama':'apel', 'asal':'sampang'}
+print (apel_dictionary)
+                            #Operator Aritmetika
+print(2+3) #tambah
+print(2*3) #perkalian
+print(20/2) #pembagian
+print(30-2) #pengurangan
+e = 25
+a = 5
+print (e % a) #sisa bagi
+                            # tanda ** adalah pangkat
+                            #tanda // pembagian bulat
+                            #cara post github
+                            #github adaalah sebuah website dan layanan berbasis clud bagi para developer untuk menyimpan data coding
